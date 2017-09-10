8ce9bcf9634e14bd074d275ba0afacaf8362b9e7
Robert Curran <ro5635@gmail.com>
2017-09-10 23:17:56 +0100

    Moved Ticket Logic to ticket Model
     On branch master
     Changes to be committed:
    	copied:     routes/tickets.js -> models/ticket.js
    	modified:   routes/tickets.js

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,180 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const uuid = require('node-uuid');
 
-const mysql = require('mysql');
-const conn = mysql.createConnection({
-    host: 'db.uk',
-    user: 'root',
-    password: '5635',
-    database: 'astonhack'
-});
-conn.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
+const ticketing = require('../models/ticket');
 
 router.get('/provision', function (req, res, next) {
 
 
     const currentBatchID = 1;
 
-    /* Begin transaction */
-    conn.beginTransaction(function (err) {
-        if (err) {
-            return res.send({Error: err});
-        }
 
-        ticketsRemain(conn, currentBatchID)
-            .then(provisionTicket)
-            .then(getRemaningTickets)
-            .then(remainingTicketsNotNegative)
-            .then(createTicket)
-            .then(function (payload) {
-                "use strict";
-                payload.conn.commit(function (err) {
-                    if (err) {
-                        conn.rollback(function () {
-                            throw err;
-                        });
-                    }
+    // Check google re-captcha
 
-                    res.send({ticket: {id: payload.ticketID}});
 
-                });
-            })
-            .catch(function (err) {
-                //The request for a ticket could not be fulfilled
-                "use strict";
+    ticketing.getTicket(currentBatchID).then(function (ticket) {
+        "use strict";
+        return res.send({ticket: {id: ticket.ID}});
 
-                conn.rollback();
-                res.send({Error: err});
-
-            });
+    })
+    .catch(function (err) {
+    "use strict";
+    return res.send({Error: err});
 
     });
-    /* End transaction */
+
 
 
 });
-
-
-function createTicket(payload) {
-    "use strict";
-
-
-    return new Promise(function (resolve, reject) {
-
-        let ticketID = uuid.v4();
-
-        //Insert a ticket into table
-        payload.conn.query('INSERT INTO Tickets (ID, AllocationDate, State, BatchID) VALUES(? , NOW(), "provisioned", ?);', [ticketID, payload.batchID],
-            function (err, result) {
-
-                if (err) {
-                    reject(err);
-
-                }
-                payload.ticketID = ticketID;
-                resolve(payload);
-
-            });
-
-    });
-
-}
-
-/**
- * This ensures that the number of remaining tickets have not been taken negative, if the number of
- * tickets is negative then there is no ticket available for this request. This is required to ensure
- * that tickets for the event are not oversold.
- *
- * @param payload   Promise chain payload
- * @returns {Promise}
- */
-function remainingTicketsNotNegative(payload) {
-    "use strict";
-
-    return new Promise(function (resolve, reject) {
-
-        getRemaningTickets(payload).then((payload) => {
-            if (payload.remainingTickets >= 0) {
-
-                return resolve(payload);
-
-            } else {
-                reject('No Tickets Available');
-            }
-        });
-    });
-
-}
-
-function ticketsRemain(connection, batchID) {
-    "use strict";
-
-    return new Promise(function (resolve, reject) {
-
-        //Build teh payload for passing down the promise chain
-        let payload = {conn: connection, batchID: batchID};
-
-        getRemaningTickets(payload).then((payload) => {
-            if (payload.remainingTickets > 0) {
-
-                return resolve(payload);
-            } else {
-                reject('No Tickets Available');
-            }
-        });
-
-    })
-}
-
-function provisionTicket(payload) {
-    "use strict";
-    let batchID = payload.batchID;
-    let connection = payload.conn;
-
-    return new Promise(function (resolve, reject) {
-
-        connection.query('UPDATE TicketBatches SET TicketsRemaining = TicketsRemaining -1 WHERE ID = ?', [batchID], function (err, result) {
-            if (err) {
-                conn.rollback(function () {
-                    return reject('Unknown Error');
-                });
-            }
-
-            let payload = {conn: connection, batchID: batchID};
-            return resolve(payload);
-
-
-        });
-
-    });
-
-}
-
-function getRemaningTickets(payload) {
-    "use strict";
-    let batchID = payload.batchID;
-    let connection = payload.conn;
-
-    return new Promise(function (resolve, reject) {
-        connection.query('SELECT TicketsRemaining FROM TicketBatches WHERE ID = ?', [batchID], function (err, result) {
-            if (err) {
-                reject('Unknown Error');
-            }
-
-            let ticketsRemaing = result[0]['TicketsRemaining'];
-
-            //Can only return one argument from a promise
-            resolve({conn: connection, batchID: batchID, remainingTickets: ticketsRemaing});
-
-        });
-
-    });
-}
 
 
 // conn.end();
